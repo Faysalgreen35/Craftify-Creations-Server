9ea7760507601dee65c2b77add846ef7f431049c
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2024-04-27 23:27:12 +0600

    get element and  post added  commit

--- index.js
+++ index.js
@@ -32,6 +32,46 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    // app.get('/coffee/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await coffeeCollection.findOne(query);
+    //   res.send(result);
+
+    // })
+
+    // app.delete('/coffee/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await coffeeCollection.deleteOne(query);
+    //   res.send(result);
+    // })
+
+    // app.put('/coffee/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) }
+    //   const options = { upsert: true };
+    //   const updatedCoffee = req.body;
+    //   const coffee = {
+    //     $set: {
+    //       name: updatedCoffee.name,
+    //       quantity: updatedCoffee.quantity,
+    //       supplier: updatedCoffee.supplier,
+    //       taste: updatedCoffee.taste,
+    //       category: updatedCoffee.category,
+    //       details: updatedCoffee.details,
+    //       photo: updatedCoffee.photo
+    //     }
+    //   }
+    //   const result = await coffeeCollection.updateOne(filter, coffee, options);
+    //   res.send(result);
+    // })
+
+    //user related api
+
+     
+
     app.post('/craftify', async (req, res) => {
         const newcraftify = req.body;
         console.log(newcraftify);
